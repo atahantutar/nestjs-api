24fe84694eef8cdd2aa3375a30872432c693bd29
atahantutar <atahantutar69@gmail.com>
2024-05-30 00:12:26 +0300

    feat: Add endpoint for updating user information

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,30 +1,39 @@
-import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('/user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @Post()
+  @Post('/create')
   create(@Body() createUserDto: CreateUserDto) {
     return this.userService.createUser(createUserDto);
   }
 
-  @Get()
+  @Get('/users')
   findAll() {
     return this.userService.findAllUser();
   }
 
-  @Get(':id')
+  @Get('/viewuser/:id')
   findOne(@Param('id') id: string) {
     return this.userService.viewUser(+id);
   }
 
-  // @Patch(':id')
-  // update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-  //   return this.userService.updateUser(+id, updateUserDto);
-  // }
+  @Patch('/updateuser/:id')
+  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+    return this.userService.updateUser(+id, updateUserDto);
+  }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
